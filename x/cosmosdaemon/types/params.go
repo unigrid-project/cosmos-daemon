26fcea37151d0371f7698c23b9cd222459b4e4bc
Torekhan Abayev <torekhan@unigrid.org>
2023-07-18 17:27:26 +0200

    Add Hedgehog parameters into module

--- x/cosmosdaemon/types/params.go
+++ x/cosmosdaemon/types/params.go
@@ -1,11 +1,25 @@
 package types
 
 import (
+	"errors"
+	fmt "fmt"
+	"strings"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
 	"gopkg.in/yaml.v2"
 )
 
 var _ paramtypes.ParamSet = (*Params)(nil)
+
+const (
+	DefaultDenom    string = "UGD"
+	DefaultAmount   uint64 = 100000000
+	DefaultMaxRate  uint64 = 9999
+	DefaultRate     uint64 = 888
+	DefaultMaxBytes uint64 = 77777
+	DefaultMaxGas   uint64 = 666
+)
 
 // ParamKeyTable the param key table for launch module
 func ParamKeyTable() paramtypes.KeyTable {
@@ -13,13 +27,27 @@ func ParamKeyTable() paramtypes.KeyTable {
 }
 
 // NewParams creates a new Params instance
-func NewParams() Params {
-	return Params{}
+func NewParams(denom string, amount uint64, maxRate uint64, rate uint64, maxBytes uint64, maxGas uint64) Params {
+	return Params{
+		Denom:    denom,
+		Amount:   amount,
+		MaxRate:  maxRate,
+		Rate:     rate,
+		MaxBytes: maxBytes,
+		MaxGas:   maxGas,
+	}
 }
 
 // DefaultParams returns a default set of parameters
 func DefaultParams() Params {
-	return NewParams()
+	return NewParams(
+		DefaultDenom,
+		DefaultAmount,
+		DefaultMaxRate,
+		DefaultRate,
+		DefaultMaxBytes,
+		DefaultMaxGas,
+	)
 }
 
 // ParamSetPairs get the params.ParamSet
@@ -29,6 +57,112 @@ func (p *Params) ParamSetPairs() paramtypes.ParamSetPairs {
 
 // Validate validates the set of params
 func (p Params) Validate() error {
+	if err := validateDenom(p.Denom); err != nil {
+		return err
+	}
+
+	if err := validateAmount(p.Amount); err != nil {
+		return err
+	}
+
+	if err := validateMaxRate(p.MaxRate); err != nil {
+		return err
+	}
+
+	if err := validateRate(p.Rate); err != nil {
+		return err
+	}
+
+	if err := validateMaxBytes(p.MaxBytes); err != nil {
+		return err
+	}
+
+	if err := validateMaxGas(p.MaxGas); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func validateAmount(i interface{}) error {
+	v, ok := i.(uint64)
+	if !ok {
+		return fmt.Errorf("invalid parameter type: %T", i)
+	}
+
+	if v == 0 {
+		return fmt.Errorf("amount must be positive: %d", v)
+	}
+
+	return nil
+}
+
+func validateDenom(i interface{}) error {
+	v, ok := i.(string)
+	if !ok {
+		return fmt.Errorf("invalid parameter type: %T", i)
+	}
+
+	if strings.TrimSpace(v) == "" {
+		return errors.New("denom cannot be blank")
+	}
+
+	if err := sdk.ValidateDenom(v); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func validateMaxRate(i interface{}) error {
+	v, ok := i.(uint64)
+	if !ok {
+		return fmt.Errorf("invalid parameter type: %T", i)
+	}
+
+	if v == 0 {
+		return fmt.Errorf("maxRate must be positive: %d", v)
+	}
+
+	return nil
+}
+
+func validateRate(i interface{}) error {
+	v, ok := i.(uint64)
+	if !ok {
+		return fmt.Errorf("invalid parameter type: %T", i)
+	}
+
+	if v == 0 {
+		return fmt.Errorf("rate must be positive: %d", v)
+	}
+
+	return nil
+}
+
+func validateMaxBytes(i interface{}) error {
+	v, ok := i.(uint64)
+	if !ok {
+		return fmt.Errorf("invalid parameter type: %T", i)
+	}
+
+	if v == 0 {
+		return fmt.Errorf("maxBytes must be positive: %d", v)
+	}
+
+	return nil
+}
+
+func validateMaxGas(i interface{}) error {
+	v, ok := i.(uint64)
+	if !ok {
+		return fmt.Errorf("invalid parameter type: %T", i)
+	}
+
+	if v == 0 {
+		return fmt.Errorf("maxGas must be positive: %d", v)
+	}
+
 	return nil
 }
 
