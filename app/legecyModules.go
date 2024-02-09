6d4fca8ba8e6d505fb5dd86e083ab1e86de48f7e
dekm <gevan73@gmail.com>
2024-02-09 14:39:44 +0100

    add missing SetRouter

--- app/legecyModules.go
+++ app/legecyModules.go
@@ -176,6 +176,7 @@ func (app *App) registerLegecyModules(appOpts servertypes.AppOptions) {
 		AddRoute(icacontrollertypes.SubModuleName, icaControllerIBCModule).
 		AddRoute(icahosttypes.SubModuleName, icaHostIBCModule)
 
+	app.IBCKeeper.SetRouter(ibcRouter)
 	// this line is used by starport scaffolding # ibc/app/module
 
 	homePath := cast.ToString(appOpts.Get(flags.FlagHome))
@@ -213,8 +214,6 @@ func (app *App) registerLegecyModules(appOpts servertypes.AppOptions) {
 		authtypes.NewModuleAddress(govtypes.ModuleName).String(),
 		wasmOpts...,
 	)
-
-	//app.IBCKeeper.SetRouter(ibcRouter)
 
 	app.ScopedIBCKeeper = scopedIBCKeeper
 	app.ScopedIBCTransferKeeper = scopedIBCTransferKeeper
