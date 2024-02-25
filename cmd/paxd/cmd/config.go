695c23a902f0e404eafe7ac567f777291528f8e5
dekm <gevan73@gmail.com>
2024-02-25 17:04:41 +0100

    add a default value for minGasPrice

--- cmd/paxd/cmd/config.go
+++ cmd/paxd/cmd/config.go
@@ -62,6 +62,7 @@ func initAppConfig() (string, interface{}) {
 	//
 	// In tests, we set the min gas prices to 0.
 	// srvCfg.MinGasPrices = "0stake"
+	srvCfg.MinGasPrices = "0ugd"
 	// srvCfg.BaseConfig.IAVLDisableFastNode = true // disable fastnode by default
 
 	customAppConfig := CustomAppConfig{
